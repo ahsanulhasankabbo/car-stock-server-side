4f105649fb9ef689da0e89dd04b9524ecd360f8c
Ahsanul Hasan Kabbo <kabboahsanulhasan@gmail.com>
2022-05-08 21:07:32 +0600

    add itmes on server side

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
@@ -9,6 +10,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorize access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+        console.log('decoded', decoded);
+        res.decoded = decoded;
+        next();
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fsvwn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,18 +34,75 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db('car-stock').collection('service');
 
-        app.get('/inventory', async(req, res) => {
+        //auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+        //services api
+        app.get('/inventory', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const inventories = await cursor.toArray();
             res.send(inventories);
         })
 
-        app.get('/inventory/:id',async(req,res)=>{
+        app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const inventory = await serviceCollection.findOne(query);
             res.send(inventory);
+        })
+
+        //add
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email };
+                const cursor = serviceCollection.find(query);
+                const add = await cursor.toArray();
+                res.send(add);
+            }
+            else{
+                res.status(403).send({ message: 'forbidden access' })
+            }
+        })
+
+        //post
+        app.post('/manageinventories', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        })
+        //delete
+        app.delete('/manageinventories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+        //put
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateQuantity = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: updateQuantity.quantity
+                }
+            };
+            const result = await serviceCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            res.send(result);
         })
     }
     finally {
